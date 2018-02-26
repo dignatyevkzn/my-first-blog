123c3a50d216e6b548a8f529da64171d4d82b04f
dignatyevkzn <dmitriy@rosexport.su>
2018-02-26 07:04:14 +0400

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'index.html')
+
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
 # Create your views here.
--- untitled2/urls.py
+++ untitled2/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf.urls import include, url
+
 from blog import views
 
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
+
 ]
